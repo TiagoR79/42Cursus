90227dac2fc60215d5ea9f890f0600b64f42f377
Tiago Ribeiro <tribeiro@c1r13s1.42lisboa.com>
2021-03-26 14:19:58 +0000

    115

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,17 +12,6 @@
 
 #include "get_next_line.h"
 
-int		ft_null(char **str, char **line)
-{
-	if ((*str)[0] == '\0')
-	{
-		free(*str);
-		*line = ft_strdup("");
-		return (0);
-	}
-	return (1);
-}
-
 int		ft_to_line(char **str, char **line)
 {
 	size_t	len;
@@ -31,8 +20,7 @@ int		ft_to_line(char **str, char **line)
 	len = 0;
 	while ((*str)[len] != '\n' && (*str)[len] != '\0')
 		len++;
-	ft_null(str, line);
-	if ((*str)[len] == '\0' || line == NULL)
+	if ((*str)[len] == '\0')
 	{
 		*line = ft_strdup(*str);
 		free(*str);
@@ -49,7 +37,7 @@ int		ft_to_line(char **str, char **line)
 	return (1);
 }
 
-void	to_name(char **str, char *buffer, int ret)
+void	buffer_to_static(char **str, char *buffer, int ret)
 {
 	char *tmp;
 
@@ -70,15 +58,14 @@ int		get_next_line(int fd, char **line)
 	char		*buffer;
 	static char	*static_string[MAX_SIZE];
 
-	if (fd < 0 || fd == 1 || fd == 2 || fd >= MAX_SIZE || line == NULL
-		|| read(fd, 0, 0) == -1 || BUFFER_SIZE <= 0)
+	if (line == NULL || read(fd, 0, 0) == -1 || BUFFER_SIZE <= 0)
 		return (-1);
 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (buffer == NULL)
 		return (-1);
 	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		to_name(&static_string[fd], buffer, ret);
+		buffer_to_static(&static_string[fd], buffer, ret);
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -12,16 +12,6 @@
 
 #include "get_next_line_bonus.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 char	*ft_strchr(const char *str, int c)
 {
 	int		i;
@@ -59,6 +49,16 @@ char	*ft_strdup(const char *s)
 	}
 	dup[i] = '\0';
 	return (dup);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
